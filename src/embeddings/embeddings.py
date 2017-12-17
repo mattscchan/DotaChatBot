3a9dca67444b525a5a0e6d058c34a56f3a6c87c0
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-17 13:08:48 -0500

    trying to find how many unique vocab words we have in corpus

--- src/embeddings/embeddings.py
+++ src/embeddings/embeddings.py
@@ -1,11 +1,24 @@
 import tensorflow as tf
 import argparse
+import csv
+from collections import Counter
 
 def main(args):
-	pass
+	word_counts = Counter()
+
+	with open(args.filename, newline='', encoding='utf-8') as f:
+		csv_reader = csv.reader(f, delimiter='\t')
+		# Each line is an example
+		for line in csv_reader:
+			for utterance in line:
+				words_list = utterance.split(' ')
+				# Track word frequencies
+				word_counts.update(words_list)
+		print(len(word_counts))
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
+	parser.add_argument('filename')
 	args = parser.parse_args()
 	main(args)
