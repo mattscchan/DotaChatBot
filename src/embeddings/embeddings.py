a7abfad848a4430c177341071f1836d75efc7cb9
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-17 13:17:17 -0500

    trying with lower-case

--- src/embeddings/embeddings.py
+++ src/embeddings/embeddings.py
@@ -6,13 +6,14 @@ from collections import Counter
 def main(args):
 	word_counts = Counter()
 	csv.field_size_limit(2147483647)
-	
+
 	with open(args.filename, newline='', encoding='utf-8') as f:
 		csv_reader = csv.reader(f, delimiter='\t')
 		# Each line is an example
 		for line in csv_reader:
 			for utterance in line:
 				words_list = utterance.split(' ')
+				words_list = [word.lower() for word in words_list]
 				# Track word frequencies
 				word_counts.update(words_list)
 		print(len(word_counts))
