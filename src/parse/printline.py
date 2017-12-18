3dede101f6d81b36cfdb99efd37740e45f5330d3
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-18 13:46:03 -0500

    printing to see if it worked

--- src/parse/printline.py
+++ src/parse/printline.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 def main(args):
 	with open(args.filename, 'r') as f:
@@ -6,6 +7,12 @@ def main(args):
 		for line in f:
 			if count == args.line:
 				print(line)
+				if args.json:
+					obj = json.loads(line)
+					print(obj['context_size'])
+					print(obj['context'])
+					print(obj['next_utt'])
+					print(obj['label'])
 				break
 			count += 1
 
@@ -13,5 +20,6 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('filename')
 	parser.add_argument('line', type=int)
+	parser.add_argument('-json', '--json', type=bool)
 	args = parser.parse_args()
 	main(args)
