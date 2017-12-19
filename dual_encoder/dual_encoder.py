77c3173cf1d88482e5175d87dfa257c93adbbca4
cerulean <williamtrung.le@gmail.com>
2017-12-18 19:41:57 -0500

    Architecture for DERNN, missing batch data

---
+++ dual_encoder/dual_encoder.py
@@ -0,0 +1,211 @@
+'''
+@authors
+William Le
+Matthew Chan
+
+@date
+2017-12-21
+
+@version
+Python 3.5.4
+TensorFlow 1.4.0
+'''
+
+import csv
+import numpy as np
+import tensorflow as tf
+from collection import namedtuple
+
+SEED = int('0xcafebabe', 16)
+
+# For reproducibility purposes
+np.random.seed(SEED)
+tf.set_random_seed(SEED)
+
+## Hyperparameters ##
+Hyper = namedtuple('Hyper', [
+        'initializer',      #tensorflow function
+        'optimizer',        #tensorflow function
+        'cell_type',        #string
+        'max_timesteps',    #floats
+        'hidden_size',
+        'num_layers',
+        'batch_size',
+        'dropout_prob',
+        'initial_lr',
+        'decay_rate',
+        'decay_steps',
+        ])
+
+## Constants ##
+Const = namedtuple('Const', [
+        'embedding_size',
+        'embedding_vocab',
+        'num_classes',
+        ])
+
+## Initialize hyperparameters and constants ##
+def create_params(params_file, number=-1):
+    '''
+    Reads a file containing the comma delimited hyperparameters, then constants on one line
+    First two arguments are tensorflow functions (initializer, optimizer)
+    Second is a string (cell_type)
+    Last three are constants
+    '''
+    with open(params_file) as f:
+        for i, line in enumerate(csv.reader(f)):
+            break if i == number else pass
+    params = [eval(f) for f in line[:2]]
+    params.append(line[2])
+    params.extend(float(v) for v in line[3:])
+    return Hyper(params[:-3]), Const(params[-1:])
+
+
+## Architecture ##
+def create_rnn(cell_type, hidden_size, dropout_prob=None, num_layers=1, batch_size=1):
+    '''
+    Intialize an RNN unit used for encoding both the context and the response vectors.
+
+    Parameters
+    ----------
+    cell_type:      string, one of {'rnn', 'gru', 'lstm'}
+    hidden_size:    int, number of hidden nodes of the RNN, also the output size
+    dropout_prob:   int, probability of zeroed out node
+    num_layers:     int, number of hidden layers for the RNN
+    batch_size:     int, size of training batch
+
+    Outputs
+    -------
+    an instance of tf.nn.rnn_cell.RNNCell
+    a initial RNN state tensor of shape (batch_size, hidden_size)
+
+    '''
+    with tf.variable_scope('RNN'):
+        cell_unit = {'rnn': tf.nn.rnn_cell.BasicRNNCell,
+                    'gru': tf.nn.rnn_cellGRUCell,
+                    'lstm': tf.nn.rnn_cell.LSTMCell}
+
+        cell = cell_unit[cell_type.lower()](hidden_size, state_is_tuple=True)
+        if dropout_prob:
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=dropout_prob)
+        if num_layers > 1:
+            cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers)
+
+        # Initialize the state tensor
+        initial_state = cell.zero_state(batch_size, tf.float32)
+
+        return cell, initial_state
+
+def create_dual_encoder(context, response, cell, state, weights, bias, initializer):
+    '''
+    Creates a single RNN instance used to encode both the context and the response vectors.
+    This is due to using a weight tied architecture for the dual encoder.
+    Multiplies the output state of both encodings and multiples them together with a learned weight matrix.
+
+    Parameters
+    ----------
+    context:        tensor(batch_size, max_timesteps, embedding_dimensions)
+    response:       tensor(batch_size, max_timesteps, embedding_dimensions)
+    cell:           RNNCell instance with the same batch_size
+    state:          intial state tensor of cell
+    weights:        weight matrix for combining the context and response output states of the RNN
+    bias:           scalar bias
+    initializer:    weights initializer for the RNN
+
+    Outputs
+    -------
+    tensor(batch_size, 1) corresponding to the logits = [(c^T)Mr] + b
+    '''
+    with tf.variable_scope('RNN', initializer=initializer()):
+        # Length of the unpadded portion of the inputs: a time_step value <= max_timesteps
+        context_length = tf.reduce_sum(tf.reduce_max(tf.sign(context), 2), 1)
+        response_length = tf.reduce_sum(tf.reduce_max(tf.sign(response), 2), 1)
+
+        _, context_state = tf.nn.dynamic_rnn(cell, context, context_length, initial_state, dtype=tf.float32)
+        _, response_state = tf.nn.dynamic_rnn(cell, response, response_length, initial_state, dtype=tf.float32)
+
+        generated_context = tf.matmul(weights, response_state)
+        similarity = tf.matmul(context_state, generated_context, transpose_a=True)
+
+        return tf.add(similary, bias, name='logits')
+
+def create_performance(labels, logits, optimizer, lr, decay_rate, decay_steps, global_step):
+    with tf.name_scope("loss"):
+        cross = tf.nn.softmax_cross_entropy_with_logits(labels=y_onehot, logits=logits)
+        loss = tf.reduce_mean(cross, name="loss")
+                    
+    with tf.name_scope("train")
+        if not (decay_rate or decay_steps):
+            decay_rate = 1.0
+            decay_steps = 1
+
+        learning_rate = tf.train.exponential_decay(lr, global_step, decay_steps, decay_rate, staircase=True)
+        optimizer = optimizer(learning_rate)
+        training = optimizer.minimize(loss, global_step=global_step)
+                                    
+    with tf.name_scope("eval"):
+        pred_onehot = tf.nn.softmax(logits)
+        pred = tf.argmax(pred, axis=1)
+        correct = tf.equal(pred, y)
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    
+    return training, accuracy
+
+def train(epochs, batch_size):
+    pass
+    
+def main(args):
+    # Load parameters from file
+    hyper, const = create_params(args.params, args.linenumber)
+    current_epoch= tf.Variable(0, trainable=False)
+
+    # Inputs
+    embeddings = tf.placeholder(tf.float32, shape=(const.vocab_size, const.embedding_size, name='embeddings'))
+    x_context = tf.placehoder(tf.int32, shape=(None, hyper.max_timesteps, const.embedding_size), name='x_context'))
+    x_response = tf.placeholder(tf.int32, shape=(None, hyper.max_timesteps, const.embedding_size), name='x_response'))
+    y = tf.placeholder(tf.int32, shape=(None, const.num_classes), name='y'))
+
+    # Formatting inputs
+    x_c_emb = tf.nn.embedding_lookup(embeddings, x_context, name='x_c_emb')
+    x_r_emb = tf.nn.embedding_lookup(embeddings, x_response, name='x_r_emb')
+    y_onehot = tf.one_hot(y, const.num_classes, name='y_onehot')
+    
+    # Initialize network
+    rnn, initial_state = create_rnn(
+            cell_type=hyper.cell_type,
+            hidden_size=hyper.hidden_size,
+            dropout_prob=hyper.dropout_prob,
+            num_layers=hyper.num_layers,
+            batch_size=hyper.batch_size)
+
+    M= tf.get_variable('M', 
+            shape=[hyper.hidden_size, hyper.hidden_size], 
+            initializer=hyper.initializer())
+
+    b= tf.get_variable('b',
+            shape=[1],
+            initializer=hyper.initializer())
+
+    logits = create_dual_encoder(
+            context=x_c_emb,
+            response=x_r_emb,
+            cell_rnn,
+            weights=M,
+            bias=b,
+            initializer=hyper.initializer)
+
+    training, accuracy = create_performance(labels=y_onehot, 
+            logits=logits, 
+            optimizer=hyper.optimizer,
+            lr=hyper.lr, 
+            decay_rate=hyper.decay_rate, 
+            decay_steps=hyper.decay_steps, 
+            global_step=current_epoch)
+
+    results = train()
+
+def __main__():
+    parser = argparse.ArgumentParser(description='Dual RNN encoder model for next utterance classification.')
+    parser.add_argument('-s', '--saved', action='store_true', default=False)
+    args = parser.parse_args()
+    main(args)
