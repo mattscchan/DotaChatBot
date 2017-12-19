a87034bc2b31b2748a7bf862d19ffc722817f89d
cerulean <williamtrung.le@gmail.com>
2017-12-19 13:25:01 -0500

    Hands

--- dual_encoder/dual_encoder.py
+++ dual_encoder/dual_encoder.py
@@ -44,30 +44,6 @@ Const = namedtuple('Const', [
         'num_classes',
         ])
 
-def parse_JSON(example):
-    parsed_ex = tf.decode_json_example(example)
-
-    return parsed_ex['context'], parsed_ex['next_utt'], parsed_ex['label']
-
-def create_dataset(filenames, parse_function, num_parallel_calls=1, batch_size=32,  shuffle_buffer=10000, num_epochs=1):
-
-    dataset = tf.data.TextLineDataset(filenames)
-    dataset = dataset.map(parse_function, num_parallel_calls=num_parallel_calls)
-
-    if num_epochs < 0:
-        dataset = dataset.repeat()
-    else:
-        dataset = dataset.repeat(num_epochs)
-
-    dataset = dataset.shuffle(shuffle_buffer)
-    dataset = dataset.padded_batch(batch_size, [3, None])
-    dataset = dataset.prefetch(10000)
-    iterator = dataset.make_initializable_iterator()
-    next_element = iterator.get_next()
-
-    return next_element, iterator
-
-
 ## Initialize hyperparameters and constants ##
 def create_params(params_file, number=-1):
     '''
@@ -110,7 +86,7 @@ def create_rnn(cell_type, hidden_size, dropout_prob=None, num_layers=1, batch_si
 
         cell = cell_unit[cell_type.lower()](hidden_size, state_is_tuple=True)
         if dropout_prob:
-            cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=dropout_prob)
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=1-dropout_prob)
         if num_layers > 1:
             cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers)
 
