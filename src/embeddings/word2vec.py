9d7dd0b4fd8e596d12bff0bac1855c0db0c60637
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-19 19:59:37 -0500

    asdf

--- src/embeddings/word2vec.py
+++ src/embeddings/word2vec.py
@@ -52,13 +52,13 @@ def load_table(vectorfile):
 #         yield center_batch, target_batch
 
 def parse_JSON(example):
-    parsed_ex = tf.decode_json_example(example)
+    parsed_ex = tf.decode_json_example(example, name="FML")
     shape = {
-                "feature": tf.VarLenFeature(tf.string)
+                "chat": tf.VarLenFeature(tf.string)
             }
     obj_ex = tf.parse_single_example(parsed_ex, shape)
     
-    return tf.sparse_tensor_to_dense(obj_ex["feature"], default_value='π')
+    return tf.sparse_tensor_to_dense(obj_ex["chat"], default_value='π')
 
 
 def create_dataset(filenames, parse_function, table, context, num_parallel_calls=1, batch_size=32,  shuffle_buffer=10000, num_epochs=1):
