681208da555226aed1b98b9bbc1dbf6dc0962222
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-20 19:40:22 -0500

    remove print so i don't kill myself

--- src/embeddings/keras_word2vec.py
+++ src/embeddings/keras_word2vec.py
@@ -36,9 +36,8 @@ def main(args):
 	word_target = []
 	word_context = []
 	labels = []
-	
+
 	for convo in data:
-		print(convo)
 		couples, tmp_labels = skipgrams(convo, vocab_size, window_size=window_size, sampling_table=sampling_table)
 		if len(couples) < 1:
 			continue
