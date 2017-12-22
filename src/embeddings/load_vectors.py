60e1d2c4cf368f69c110461b4986728fc56cc7ec
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-21 19:27:39 -0500

    asdf

---
+++ src/embeddings/load_vectors.py
@@ -0,0 +1,19 @@
+import gensim, logging, argparse, json
+from gensim.models.keyedvectors import KeyedVectors
+
+def main(args):
+	model = KeyedVectors.load(args.vectors, binary=False)
+
+	if args.target == '1':
+		with open('test.txt', 'r', encoding='utf-8') as f:
+			for line in f:
+				print(model.similar_by_word(line))
+	else:
+		model.similar_by_word(args.target)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('vectors')
+	parser.add_argument('target')
+	args = parser.parse_args()
+	main(args)
